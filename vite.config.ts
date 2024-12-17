1014aab36ed8703bae718c5b999a4c72cf989c06
Daniel Košč <daniel.kosc@gmail.com>
2024-12-18 00:42:29 +0100

    Fixing Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import {componentTagger} from "lovable-tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => ({
-    base: "/cybertraverse-web/",
-    plugins: [
-        react(),
-        mode === 'development' &&
-        componentTagger(),
-    ].filter(Boolean),
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "https://danyk20.github.io/cybertraverse-web/",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 }));
