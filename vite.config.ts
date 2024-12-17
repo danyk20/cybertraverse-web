638347d66a6e0833624221362454c279f33d1709
Daniel Košč <daniel.kosc@gmail.com>
2024-12-18 00:31:04 +0100

    Fixing Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {componentTagger} from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
-    base: "/cybertraverse-web",
+    base: "/cybertraverse-web/",
     plugins: [
         react(),
         mode === 'development' &&
