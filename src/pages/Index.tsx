ff667db1e626d4f00d9d71075d7d159d1cf0c553
Daniel Košč <daniel.kosc@gmail.com>
2024-12-17 15:44:05 +0100

    Remove animated background

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/Hero";
 import Interests from "@/components/Interests";
 import Contact from "@/components/Contact";
-import AnimatedBackground from "@/components/AnimatedBackground";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background text-foreground relative">
-      <AnimatedBackground />
       <Hero />
       <Interests />
       <Contact />
