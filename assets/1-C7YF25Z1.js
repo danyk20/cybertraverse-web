34cb9b7b3ba66e08b6915fe69f0545264a307f04
Daniel Košč <daniel.kosc@gmail.com>
2025-02-28 15:05:10 +0100

    Updates

--- assets/1-C7YF25Z1.js
+++ assets/1-C7YF25Z1.js
@@ -1,4 +1,4 @@
-import{c as s,u as o,j as e,m as r,A as i,S as a}from"./index-cLK6lYwe.js";/**
+import{c as s,u as o,j as e,m as r,A as i,S as a}from"./index-BrCmgHKA.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
