f1acc203a9a803dbd75ea49911e3870df6c25782
Daniel Košč <daniel.kosc@gmail.com>
2025-03-28 19:58:41 +0100

    Updates

--- assets/house-CUeEZ5G2.js
+++ assets/house-CUeEZ5G2.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BMXQdmpW.js";/**
+import{c as a}from"./index-PnUa3698.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
