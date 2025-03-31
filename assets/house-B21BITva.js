c110829c3dfb93251035d01af78aece1637f461b
Daniel Košč <daniel.kosc@gmail.com>
2025-03-31 21:24:16 +0200

    Updates

--- assets/house-B21BITva.js
+++ assets/house-B21BITva.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-PnUa3698.js";/**
+import{c as a}from"./index-Bw3NcWHF.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
